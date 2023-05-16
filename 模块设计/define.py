f0918091b08073c4195a92535ec05ebb8d485b15
sunhaowei <1390736877@qq.com>
2023-05-16 18:07:15 +0800

    up
    up

---
+++ 模块设计/define.py
@@ -0,0 +1,29 @@
+import random
+WINWIDTH = 640
+WINHEIGHT = 480
+ROW = 3
+COL = 3
+BLANK = None
+#-------颜色-------
+DARKGRAY = (60,60,60)
+WHITE = (255,255,255)
+YELLOW = (255,255,193)
+GRAY = (128,128,128)
+BRIGHTBLUE = (138,228,221)
+#-------颜色变量------
+BLANKCOLOR = DARKGRAY
+MSGCOLOR = WHITE
+BTCOLOR = YELLOW
+BTTEXTCOLOR = GRAY
+BDCOLOR = BRIGHTBLUE
+
+#-------静态常量-------
+BLOCKSIZE = 80 #滑块边长
+FPS = 60
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+NEWGAME = 'newgame'
+AUTOMOVE = random.randint(50,100)
+
